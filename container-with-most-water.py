86a1ad4a740f79d6d38036ec6aaa1c60c01849de
Stephen O'Sullivan <osullisg@gmail.com>
2015-07-10 20:42:44 -0700

    finished

--- container-with-most-water.py
+++ container-with-most-water.py
@@ -2,3 +2,23 @@ class Solution:
     # @param {integer[]} height
     # @return {integer}
     def maxArea(self, height):
+        left = 0
+        right = len(height)-1
+        maxArea = min(height[left],height[right])*(right-left)
+        while right != left:
+            if height[left]<height[right]:
+                left += 1
+            else:
+                right -= 1
+            maxArea = max(maxArea, (right-left)*min(height[left],height[right]))
+        return maxArea
+    
+    # O(n^2) too slow!!        
+    # def maxArea(self, height):
+    #     ptr1 = height
+    #     ptr2 = height
+    #     maxArea = 0
+    #     for ptr1 in range(len(height)-1):
+    #         for ptr2 in range(ptr1+1,len(height)):
+    #             maxArea = max(maxArea, (ptr2-ptr1)*min(height[ptr1],height[ptr2]))
+    #     return maxArea
