f2d0ce10584369fac5bc3265f48aaeb177b346ff
Stephen O'Sullivan <osullisg@gmail.com>
2015-07-22 15:45:04 -0700

    84ms

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,18 @@
+__author__ = 'stephenosullivan'
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer}
+    def maxDepth(self, root):
+        if root:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        else:
+            return 0
+
