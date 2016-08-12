8a4c18cba35dba1aa2c0a1152d587e4d7de0e081
Stephen O'Sullivan <osullisg@gmail.com>
2016-08-12 11:26:42 -0700

    80ms: 3.43%

---
+++ Binary_tree_post_order_traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        self.output = []
+        self.recursivePostTrav(root)
+        return self.output
+
+    def recursivePostTrav(self, root):
+        if root:
+            self.recursivePostTrav(root.left)
+            self.recursivePostTrav(root.right)
+            self.output.append(root.val)
